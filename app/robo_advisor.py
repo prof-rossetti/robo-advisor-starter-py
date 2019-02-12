89d24e08817d6db480dfb59a71a6b68aa1a5a9f6
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-11 21:48:00 -0500

    Provide a more complete example output

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,23 +3,40 @@ import json
 import os
 import requests
 
-load_dotenv()  # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
+load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
 
 # see: https://www.alphavantage.co/support/#api-key
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+print("API KEY: " + api_key) # TODO: remove or comment-out this line after you have verified the environment variable is getting read properly
 
-symbol = "NFLX" # TODO: capture user input
+symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
 
-# see: https://www.alphavantage.co/documentation/#daily
-# TODO: assemble the request url to get daily data for the given stock symbol
+# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
+# TODO: assemble the request url to get daily data for the given stock symbol...
 
-# TODO: issue a "GET" request to the specified url, and store the response in a variable
+# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
 
-# TODO: parse the JSON response
+# TODO: further parse the JSON response...
 
-# TODO: traverse the nested response data structure to find the latest closing price
+# TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
 latest_price_usd = "$100,000.00"
 
+#
+# INFO OUTPUTS
+#
+
+# TODO: write response data to a CSV file
+
+# TODO: further revise the example outputs below to reflect real information
 print("-----------------")
-print(f"SYMBOL: {symbol}")
+print(f"STOCK SYMBOL: {symbol}")
+print("RUN AT: 11:52pm on June 5th, 2018")
+print("-----------------")
+print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
 print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
+print("RECENT AVERAGE HIGH CLOSING PRICE: $101,000.00")
+print("RECENT AVERAGE LOW CLOSING PRICE: $99,000.00")
+print("-----------------")
+print("RECOMMENDATION: Buy!")
+print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+print("-----------------")
